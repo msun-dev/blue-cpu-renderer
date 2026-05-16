4693689efc28b57d6a87ac33b71f231d9b43d8c3
User <gmchofen@gmail.com>
2026-05-16 12:48:51 +0500

    submodule added: Added BLUE-CPU repo as a submodule and removed previous files

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,8 @@ flags_debug="-ggdb -g3 -O0"
 flags_ray="-lraylib -lGL -lm -lpthread -ldl -lrt -lX11"
 
 debug() {
-	cc -c ./include/emu-blue/cpu.c -o ./build/cpu.o $flags_general $flags_debug
+	cc -c ./include/blue-cpu/src/cpu.c \
+	   -o ./build/cpu.o $flags_general $flags_debug
 	cc -c ./src/main.c -o ./build/main.o $flags_general $flags_debug $flags_ray
 	cc ./build/main.o ./build/cpu.o -o ./bin/main $flags_general $flags_ray
 }
