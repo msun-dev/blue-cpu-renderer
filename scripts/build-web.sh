879852aa33551594fe4ec449c836f4882251627e
User <gmchofen@gmail.com>
2026-05-22 13:36:18 +0500

    IC: web-build script

---
+++ scripts/build-web.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+[ ! -f "./bin/" ] && mkdir -v ./bin/ 2>/dev/null
+[ ! -f "./build/" ] && mkdir -v ./build/ 2>/dev/null
+rm -f ./build/*
+
+flags_cc="--std=gnu11 -Wall -Wpedantic -Wextra -Os"
+flags_emcc="$RAYLIB/libraylibweb.a \
+            -I. -I$RAYLIB/ \
+            -L. -L$RAYLIB/ \
+            -s ASYNCIFY -s USE_GLFW=3 -DPLATFORM-WEB \
+            --shell-file $RAYLIB/minshell.html "
+
+build() {
+	emcc -o ./bin/main-web.html \
+        ./src/main.c \
+        ./include/blue-cpu/src/cpu.c $flags_cc $flags_emcc
+}
+
+build
